c5aaeaffc5d0e2d5cca513b08ea951e82b50ac7c
aravindhr22 <aravindh22022005@gmail.com>
2026-09-13 13:10:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -239,7 +239,7 @@ qualified human HR professionals.
 """
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
+        model="gemini-3.5-flash-lite",
         contents=prompt
     )
 
